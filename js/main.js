d978c179914cd962a066222389c233991e615be5
ThomasBackers <thomasbackers@gmail.com>
2021-09-18 22:32:47 +0200

    first tests done seems OK =p

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@ const randomIntegerFrom1ToMax = (max) => {
 }
 
 const setBombingCellsIndeces = (numberOfBombs) => {
+    const cellsLength = document.getElementsByClassName("cell").length;
     let bombingCellsIndeces = [];
     for (let i = 0, randomInteger = 0, previousInteger = 0; i < numberOfBombs; i++) { 
         while (randomInteger === previousInteger) {
-            randomInteger = randomIntegerFrom1ToMax(100);
+            randomInteger = randomIntegerFrom1ToMax(cellsLength);
         }
         bombingCellsIndeces.push(randomInteger);
         previousInteger = randomInteger;
@@ -14,12 +15,18 @@ const setBombingCellsIndeces = (numberOfBombs) => {
     return bombingCellsIndeces;
 }
 
-const getBombingCells = () => {
+const getBombingCellsContents = (numberOfBombs) => {
     let cellsContents = document.getElementsByClassName("cell-content");
-    const bombingCellsIndeces = setBombingCellsIndeces(15);
+    const bombingCellsIndeces = setBombingCellsIndeces(numberOfBombs);
     let bombingCellsContents = [];
     for (let index of bombingCellsIndeces) {
         bombingCellsContents.push(cellsContents[index]);
     }
     return bombingCellsContents;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log(randomIntegerFrom1ToMax(100));
+    console.log(setBombingCellsIndeces(15));
+    console.log(getBombingCellsContents(15));
+})
