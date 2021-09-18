4e35e4409a6b8eb1b55e42f2f2d896aed64f0f7b
ThomasBackers <thomasbackers@gmail.com>
2021-09-18 19:39:12 +0200

    on my way for JS

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const randomIntegerFrom1ToMax = (max) => {
+    return Math.ceil(Math.random() * max);
+}
+
+const setBombingCellsIndeces = (numberOfBombs) => {
+    const bombingCellsIndeces = [];
+    for (let i = 0; i < numberOfBombs; i++) {
+        ;
+    }
+}
+
+const setBombingCellsUp = () => {
+    let cellsContents = document.getElementsByClassName("cell-content");
+}
