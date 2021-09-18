562bda1895b3fa3587e8a10ffd75c8d9b78c69da
ThomasBackers <thomasbackers@gmail.com>
2021-09-19 00:35:04 +0200

    added eight CSS numbers classes & finished bombing field JS part

--- js/main.js
+++ js/main.js
@@ -1,40 +1,50 @@
+// SET BOMBING CELLS
 const randomIntegerFrom0ToMax = max => {
+    //return a random number between 0 and max
     return Math.floor(Math.random() * max);
 };
 
-const setBombingCellsIndeces = numberOfBombs => {
+const setBombsIndeces = numberOfBombs => {
+    //return a random indeces list for random picking in a span elements list
     const cellsLength = document.getElementsByClassName("cell").length;
-    let bombingCellsIndeces = [];
+    let BombsIndeces = [];
     for (let i = 0, randomInteger = -1, previousInteger = -1; i < numberOfBombs; i++) {
         while (randomInteger === previousInteger) {
             randomInteger = randomIntegerFrom0ToMax(cellsLength);
         }
-        bombingCellsIndeces.push(randomInteger);
+        BombsIndeces.push(randomInteger);
         previousInteger = randomInteger;
     }
-    return bombingCellsIndeces;
+    return BombsIndeces;
 };
 
 const getBombingCellsContents = numberOfBombs => {
+    //return a random span elements list to put bombs inside them
     let cellsContents = document.getElementsByClassName("cell-content");
-    const bombingCellsIndeces = setBombingCellsIndeces(numberOfBombs);
+    const BombsIndeces = setBombsIndeces(numberOfBombs);
     let bombingCellsContents = [];
-    for (let index of bombingCellsIndeces) {
+    for (let index of BombsIndeces) {
         bombingCellsContents.push(cellsContents[index]);
     }
     return bombingCellsContents;
 };
 
-const setBombingCells = (numberOfBombs) => {
+const setAndGetBombingCells = numberOfBombs => {
+    //put bombs inside random span elements
+    //also return the list of bombing cells
     const bombingCellsContents = getBombingCellsContents(numberOfBombs);
+    let bombingCells = [];
     for (let bombingCellsContent of bombingCellsContents) {
         bombingCellsContent.classList.add("fas", "fa-bomb");
         bombingCellsContent.classList.remove("hidden-cell-content");
+        bombingCells.push(bombingCellsContent.parentNode);
     }
+    return bombingCells;
 };
 
+// SET NUMBERS
+
+// TESTS
 document.addEventListener("DOMContentLoaded", () => {
-    console.log(setBombingCellsIndeces(15));
-    console.log(getBombingCellsContents(15));
-    setBombingCells(15);
+    console.log(setAndGetBombingCells(15));
 });
