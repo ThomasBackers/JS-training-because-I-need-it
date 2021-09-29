bab1a9dd0d6f6ea3a35505f2a28c01f7b653e2d6
ThomasBackers <thomasbackers@gmail.com>
2021-09-29 10:09:15 +0200

    comments & docstrings added for the JS code to be easier to read

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
+/*
+You'll have a way better understanding of the code if you check:
+- the HTML file when I try to catch cells, cells contents, elements etc.
+- the CSS file if you see me play with the classLists
+*/
+
 // ---------- SET BOMBING CELLS ----------
+/** returns a random number between 0 and max */
 const randomIntegerFrom0ToMax = max => {
-    //return a random number between 0 and max
     return Math.floor(Math.random() * max);
 };
 
+/** returns a random indeces list for random picking in a span elements list */
 const setBombsIndeces = numberOfBombs => {
-    //return a random indeces list for random picking in a span elements list
     const cellsLength = document.getElementsByClassName("cell").length;
     const BombsIndeces = [];
+    let randomInteger;
     for (let i = 0; i < numberOfBombs; i++) {
         randomInteger = randomIntegerFrom0ToMax(cellsLength);
+        // I don't want to have two bombs on the same cell so let's avoid to get two times the same index for bomb placement
         while (BombsIndeces.includes(randomInteger)) {
             randomInteger = randomIntegerFrom0ToMax(cellsLength);
         }
@@ -18,8 +26,8 @@ const setBombsIndeces = numberOfBombs => {
     return BombsIndeces;
 };
 
+/** returns a random span elements list to put bombs inside them */
 const getBombingCellsContents = numberOfBombs => {
-    //return a random span elements list to put bombs inside them
     const cellsContents = document.getElementsByClassName("cell-content");
     const BombsIndeces = setBombsIndeces(numberOfBombs);
     const bombingCellsContents = [];
@@ -29,21 +37,23 @@ const getBombingCellsContents = numberOfBombs => {
     return bombingCellsContents;
 };
 
+/**
+ * puts bombs inside random span elements
+ * also returns the list of bombing cells
+ */
 const setAndGetBombingCells = numberOfBombs => {
-    //put bombs inside random span elements
-    //also return the list of bombing cells
     const bombingCellsContents = getBombingCellsContents(numberOfBombs);
     const bombingCells = [];
-    for (let bombingCellsContent of bombingCellsContents) {
-        bombingCellsContent.classList.add("fas", "fa-bomb");
-        bombingCells.push(bombingCellsContent.parentNode);
+    for (let bombingCellContent of bombingCellsContents) {
+        bombingCellContent.classList.add("fas", "fa-bomb");
+        bombingCells.push(bombingCellContent.parentNode);
     }
     return bombingCells;
 };
 
 // ---------- SET NUMBERS ----------
+/** return a particular cell through its relative position to current cell */
 const targetCell = (currentCell, relativeTargetPosition) => {
-    //return a particular cell through its relative position to current cell
     if (relativeTargetPosition > 0) {
         for (let i = 0; i < relativeTargetPosition; i++) {
             currentCell = currentCell.nextElementSibling;
@@ -56,8 +66,8 @@ const targetCell = (currentCell, relativeTargetPosition) => {
     return currentCell;
 };
 
+/** return the list of the adjacent cells of the parameter cell */
 const getAdjacentCells = currentCell => {
-    //return the list of adjacent cells of the current one
     const adjacentCells = [],
         cellClasses = currentCell.classList;
     let topCell,
@@ -68,6 +78,15 @@ const getAdjacentCells = currentCell => {
         bottomLeftCell,
         leftCell,
         topLeftCell;
+    /* 
+    I had to do that huge switch as I have to check what's the cell's position
+    in the grid to avoid to retrieve an off-field cell (which would be null) 
+    or in an unwanted position (tipically when the cell is on the right border
+    you don't want to retrieve a cell on the right or I'll have a weird position).
+    -------------------------------------------------------------------------------
+    To better understand it, remember: When I get the cells they are in a kind of
+    a list. So I move on a single dimension object (and the grid has 2 dimensions).
+    */
     switch (true) {
         case cellClasses.contains("top"):
             bottomCell = targetCell(currentCell, 10);
@@ -146,14 +165,20 @@ const getAdjacentCells = currentCell => {
     return adjacentCells;
 };
 
+/** sets the numerical cells */
 const setField = numberOfBombs => {
-    //set the numerical cells
+    // Here I have to make a +1 on each cell around the bombs
+    // First I set & get the bombs
     const bombingCells = setAndGetBombingCells(numberOfBombs);
     for (let bombingCell of bombingCells) {
+        // Then for every bomb I get its adjacents cells
         const adjacentCells = getAdjacentCells(bombingCell);
         for (let adjacentCell of adjacentCells) {
+            // Then I get the contents of those cells
             const cellContent = adjacentCell.firstElementChild;
+            // I have to change their classes too
             const classes = cellContent.classList;
+            // And there we go:
             switch (cellContent.innerHTML) {
                 case "7":
                     cellContent.innerHTML = "8";
@@ -202,23 +227,33 @@ const setField = numberOfBombs => {
 };
 
 // ---------- CELLS BEHAVIOR ----------
+/** reveals a cell */
 const revealCell = cell => {
-    cell.classList.remove("hidden-cell")
+    cell.classList.remove("hidden-cell");
     cell.firstElementChild.classList.remove("hidden-cell-content");
-    //return cell.firstElementChild.classList;
 };
 
+/** gets the safe adjacent cells of the current one */
 const getSafeAdjacents = cell => {
     const safeCells = [];
     const adjacentCells = getAdjacentCells(cell);
     for (let adjacentCell of adjacentCells) {
-        if (!adjacentCell.firstElementChild.classList.contains("fa-bomb") && adjacentCell.classList.contains("hidden-cell")) {
+        if (
+            !adjacentCell.firstElementChild.classList.contains("fa-bomb") &&
+            adjacentCell.classList.contains("hidden-cell")
+        ) {
             safeCells.push(adjacentCell);
         }
     }
     return safeCells;
 };
 
+/**
+ * reveals the safe cells
+ * but if there are blank cells in them (I mean without any number)
+ * the function has to get their safe adjacent cells too
+ * and to do all its routine again with 'em
+ */
 const revealSafeCells = safeCells => {
     for (let safeCell of safeCells) {
         revealCell(safeCell);
@@ -230,14 +265,22 @@ const revealSafeCells = safeCells => {
 };
 
 // ---------- ATTACH EVENTS ----------
+/** attachs the clicks events to the cells */
 const attachEvents = () => {
     const cells = document.getElementsByClassName("cell");
     for (let cell of cells) {
-        if (cell.firstElementChild.classList.contains("fa-bomb") || cell.firstElementChild.classList.contains("number")) {
+        // if the cell contains a bomb or a number
+        if (
+            cell.firstElementChild.classList.contains("fa-bomb") ||
+            cell.firstElementChild.classList.contains("number")
+        ) {
+            // I simply want to reveal that cell on click
             cell.addEventListener("click", () => {
                 revealCell(cell);
             });
         } else {
+            // but if it's a blank one I want to do the full routine:
+            // reveal, check for adjacents & reveals recursively the other blank cells
             cell.addEventListener("click", () => {
                 revealCell(cell);
                 const safeAdjacents = getSafeAdjacents(cell);
